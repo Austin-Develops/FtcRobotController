9da84ecd5bbb24c88ec8019a1e13cff76a2d037f
Austin-Develops <94494977+Austin-Develops@users.noreply.github.com>
2021-11-29 18:00:27 -0700

    Update DriveController.java

--- TeamCode/src/main/java/ca/webber/ftc/helpers/DriveController.java
+++ TeamCode/src/main/java/ca/webber/ftc/helpers/DriveController.java
@@ -2,12 +2,28 @@ package ca.webber.ftc.helpers;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class DriveController {
 
     private DcMotor leftFront, rightFront, leftBack, rightBack;
+    private Servo openBucket, tiltBucket;
+    private DcMotor boxArm;
+    private final int[] OPEN_BUCKET_BOUNDARIES = {-1, 1}, TILT_BUCKET_BOUNDARIES = {-1, 1};
+    private final double[] BOX_ARM_BOUNDARIES = {0, 2000};
 
-    public DriveController (DcMotor leftFront, DcMotor rightFront, DcMotor leftBack, DcMotor rightBack) {
+    private boolean isBucketOpen;
+
+    public DriveController (
+            DcMotor leftFront,
+            DcMotor rightFront,
+            DcMotor leftBack,
+            DcMotor rightBack,
+            Servo openBucket,
+            Servo tiltBucket,
+            DcMotor boxArm
+    ) {
+
         this.leftFront = leftFront;
         this.rightFront = rightFront;
         this.leftBack = leftBack;
@@ -17,6 +33,15 @@ public class DriveController {
         this.rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         this.leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
         this.rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        this.openBucket = openBucket;
+        this.tiltBucket = tiltBucket;
+        this.boxArm = boxArm;
+
+        this.openBucket.setPosition(OPEN_BUCKET_BOUNDARIES[0]);
+        this.tiltBucket.setPosition(OPEN_BUCKET_BOUNDARIES[0]);
+
+        this.isBucketOpen = true;
     }
 
     public void update (double x, double y, double r) {
@@ -41,6 +66,28 @@ public class DriveController {
 
 //         optionally
          return Math.tanh(a);
+    }
+
+    private void moveArm(double y) {
+        if (y < 0) boxArm.setPower((boxArm.getCurrentPosition() <= BOX_ARM_BOUNDARIES[0]) ? 0: y);
+        else boxArm.setPower((boxArm.getCurrentPosition() >= BOX_ARM_BOUNDARIES[1]) ? 0: y);
+    }
+
+    private void toggleOpenBucket() {
+        isBucketOpen = !isBucketOpen;
+        openBucket.setPosition(OPEN_BUCKET_BOUNDARIES[isBucketOpen ? 0: 1]);
+        // open is the first index
+    }
+
+    private double sigmoid(double x) {
+        return 1 / (1 + Math.pow(Math.E, -x));
+    }
+
+    private void updateBucketTilt(double rot) {
+        double currPos = tiltBucket.getPosition();
+        double targetPos = currPos + (0.001 * (sigmoid(rot) - 0.5));
+        // This is really a questionable thought. Forget the extra vars, I thought I had an idea
+        tiltBucket.setPosition(targetPos);
     }
 
 }
